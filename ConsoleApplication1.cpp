75828ba451cf3a1731df69ba252ff57f2ba472e6
sawada428 <k022g1063@g.neec.ac.jp>
2022-11-18 09:19:48 +0900

    ローカルでコンフリクト変更

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -5,9 +5,10 @@
 
 int main()
 {
-    std::cout << "Hello GitHub World\n";   
+    std::cout << "Hello GitHub World\n";
 
-    std::cout << "GitHub Clone changed\n";
+    std::cout << "Conflict changed\n";
+	
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
