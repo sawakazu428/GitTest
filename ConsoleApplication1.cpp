bc986d8d455482e59e0e5bb5e86353ee52994c99
sawada <115197611+sawakazu428@users.noreply.github.com>
2022-11-18 09:21:30 +0900

    リモートリポジトリでコンフリクト変更

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -7,13 +7,7 @@ int main()
 {
     std::cout << "Hello GitHub World\n";   
 
-    std::cout << "Hello Branch changed\n";
-
-	//ブロックサイズ
-	const int BlockSize = 32;
-	//マップの大きさ
-	const int MapSizeX = 25;
-	const int MapSizeY = 20;
+    std::cout << "GitHub Clone changed\n";
 	
 }
 
